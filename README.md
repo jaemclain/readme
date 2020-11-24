1ffc51ce8c0ad82cf22b2f4f45c2c8aa3d7e7de2
Jae McLain <jae.mclain@gmail.com>
2020-11-23 19:02:31 -0800

    fixed no data entry

--- README.md
+++ README.md
@@ -1,25 +1,25 @@
 
- # Title
- 
- ## Description 
- This app is used to generate a ReadME file within your project.
-         
- ## Table of Contents
- * [Installation](#installation)
- * [Usage](#usage)
- * [Credits](#credits)
- * [License](#license)
-             
- ## Installation
- Clone the repo using the link, open your terminal and install dependencies (npm i), then type "node index.js" in your terminal and answer the questions when prompted.
-                  
- ## Usage 
- Run "node index.js" in terminal;
- Answer questions when prompted.
-         
- ## License
- 
-        
- ## Credits
+# test
 
-          
+## Description 
+tes
+        
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+            
+## Installation
+test
+                 
+## Usage 
+test
+        
+## License
+N/A
+       
+## Credits
+GitHub usernames:
+test
+         
--- index.js
+++ index.js
@@ -1,89 +1,84 @@
+//installing inquirer and fs to the index.js
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt([
+inquirer.prompt ([
     {
-        name: "Title",
-        message: "What is the name of the project?",
+        name: "title",
+        message: "What is the title of the project?",
         type: "input"
     },
     {
-        name: "Description",
-        message: "Please describe your project.",
+        name: "description",
+        message: "What is a description of this project?",
         type: "input"
     },
     {
-        name: "Table of Contents",
-        message: "Please list content of your project",
+       name: "installation",
+       message: "What are the steps for a user to install the project",
+       type: "input" 
+    },
+    {
+        name: "usage",
+        message: "What are the steps to use this project?",
         type: "input"
     },
     {
-        name: "Installation",
-        message: "Please describe how to install your project",
-        type: "input"
-    },
-    {
-        name: "Usage",
-        message: "Please describe how to use your project",
-        type: "input"
-    },
-    {
-        name: "License",
-        message: "Please choose a license.",
+        name: "license",
+        message: "Which license do you want your project covered under?",
         type: "list",
-        choices: ["MIT", "Apache 2.0", "GNU GPL v3", "N/A"]
+        choices: ['GNU AGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT Lincse', 'N/A']
     },
     {
-        name: "Contributing",
-        message: "Please list others who worked on this project",
+        name: "contribution",
+        message: "Who else worked on this project with you?",
+        type: "input",
+    },
+    {
+        name: "tests",
+        message: "Please explain the test that you conducted.",
         type: "input"
     },
     {
-        name: "Tests",
-        message: "Please explain the test(s) you conducted.",
+        name: "questions",
+        message: "What additional questions are there for the project?",
         type: "input"
-    },
-    {
-        name: "Questions",
-        message: "Are there any additional questions?",
-        type: "input"
-    },
-])
-.then(function(answer){
-    function generateMD(answer){
-        return `
- # ${answer.title}
- 
- ## Description 
- ${answer.description}
-         
- ## Table of Contents
- * [Installation](#installation)
- * [Usage](#usage)
- * [Credits](#credits)
- * [License](#license)
-             
- ## Installation
- ${answer.installation}
-                  
- ## Usage 
- ${answer.usage}
-         
- ## License
- ${answer.license}
-        
- ## Credits
- GitHub usernames:
- ${answer.contribution}
-          `
- }
-
-//  Generate Page
- fs.writeFile("README.md", generateMD(answer), function(err) {
-    if (err) {
-        console.log(err)
     }
-    console.log("Done!")
-});
+]).then(function(answer) {
+    //Creating a function to generate the md file
+    function generateMD(answer) {
+       return `
+# ${answer.title}
+
+## Description 
+${answer.description}
+        
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+            
+## Installation
+${answer.installation}
+                 
+## Usage 
+${answer.usage}
+        
+## License
+${answer.license}
+       
+## Credits
+GitHub usernames:
+${answer.contribution}
+         `
+}
+    //Creating new page
+    fs.writeFile("README.md", generateMD(answer), function(err) {
+        if (err) {
+            console.log(err)
+        }
+        console.log("Success!")
+    });
 
 })
